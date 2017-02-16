e0b38c482841571263d07381c7b3412f665ccc3c
tkea <tkea@users.noreply.github.com>
2017-02-16 14:47:21 +0100

    Begin work on elevator state machine

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,21 @@ extern crate elevator;
 
 use elevator::elevator_driver::elev_io::*;
 
+// State
+enum State {
+    Idle,
+    Running,
+    DoorOpen
+}
+
 // ElevatorData...
 struct Elevator {
     io: ElevIo,
     current_direction: MotorDir,
-    order: Option<Floor>,
     state: State,
+    orders_up: [bool; N_FLOORS],
+    orders_down: [bool; N_FLOORS],
+    orders_internal: [bool; N_FLOORS],
 }
 
 impl Elevator {
@@ -20,8 +29,10 @@ impl Elevator {
         let elevator = Elevator {
             io: elevator_io,
             current_direction: MotorDir::Stop,
-            order: None,
             state: State::Idle,
+            orders_up: [false; N_FLOORS],
+            orders_down: [false; N_FLOORS],
+            orders_internal: [false; N_FLOORS],
         };
 
         return elevator;
@@ -44,47 +55,119 @@ impl Elevator {
         self.io.set_door_light(Light::Off).unwrap();
     }
 
-    // Transition Idle => Idle
-    fn change_direction(&mut self) {
-        let new_direction = match self.current_direction {
-            MotorDir::Up => MotorDir::Down,
-            MotorDir::Down => MotorDir::Up,
-            _ => self.current_direction,
+    fn new_floor_order(&mut self, button: Button){
+        match button {
+            Button::CallUp(Floor::At(floor)) => self.orders_up[floor] = true,
+            Button::CallDown(Floor::At(floor)) => self.orders_down[floor] = true,
+            Button::Internal(Floor::At(floor)) => self.orders_internal[floor] = true,
+            _ => {}
+        }
+    }
+
+    fn get_orders_in_current_direction(&self) -> [bool; N_FLOORS] {
+        let orders_in_current_direction = match self.current_direction {
+            MotorDir::Up => self.orders_up,
+            MotorDir::Down => self.orders_down,
+            _ => self.orders_up,
         };
 
-        self.io.set_motor_dir(new_direction).unwrap();
-        self.current_direction = new_direction;
+        return orders_in_current_direction;
     }
 
-    fn is_floor_ordered(&self) -> bool {
-        return true;
-    }
-}
+    fn should_stop(&self) -> bool {
+        let current_floor = match self.io.get_floor_signal().unwrap() {
+            Floor::Between => return false,
+            Floor::At(floor) => floor,
+        };
 
-// State Machine
-enum State {
-    Idle,
-    Running,
-    DoorOpen
+        if self.orders_internal[current_floor] {
+            return true;
+        }
+
+        let orders_in_current_direction = self.get_orders_in_current_direction();
+
+        if orders_in_current_direction[current_floor] {
+            return true;
+        }
+
+        return false;
+    }
+
+    fn orders_in_direction(&self, direction: MotorDir) -> bool {
+        let current_floor = match self.io.get_floor_signal().unwrap() {
+            Floor::Between => return true,
+            Floor::At(floor) => floor,
+        };
+
+        let orders_in_current_direction = self.get_orders_in_current_direction();
+
+        let (lower_bound, upper_bound) = match direction {
+            MotorDir::Down => (0, current_floor),
+            _ => (current_floor, N_FLOORS-1),
+        };
+
+        for floor in lower_bound..upper_bound {
+            if self.orders_internal[floor] || orders_in_current_direction[floor] {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    fn set_direction(&mut self) {
+        if self.orders_in_direction(self.current_direction) {
+            self.io.set_motor_dir(self.current_direction);
+            return;
+        }
+
+        let opposite_direction = match self.current_direction {
+            MotorDir::Down => MotorDir::Up,
+            _ => MotorDir::Down
+        };
+
+        if self.orders_in_direction(opposite_direction) {
+            self.io.set_motor_dir(opposite_direction);
+            self.current_direction = opposite_direction; // Update current direction
+            return;
+        }
+
+        // No orders in any direction, so just stop
+        self.io.set_motor_dir(MotorDir::Stop);
+    }
 }
 
 
 // State: Idle
-fn event_idle(floor: Floor) {
+fn event_at_floor(elevator: &mut Elevator) {
+    if elevator.should_stop() {
+        elevator.stop_and_open_doors();
+    } else {
+        elevator.set_direction();
+    }
+}
 
+fn event_new_floor_order(elevator: &mut Elevator, button: Button){
+    elevator.new_floor_order(button);
+}
+
+fn event_door_should_close(elevator: &mut Elevator) {
+    elevator.set_direction();
 }
 
 fn main() {
-    let elevator = Elevator::new();
+    let mut elevator = Elevator::new();
 
     loop {
+        if let Floor::At(floor) = elevator.io.get_floor_signal().unwrap() {
+            event_at_floor(&mut elevator);
+        }
 
-
-        /*if let Signal::High = elevator.get_stop_signal()
+        if let Signal::High = elevator.io.get_stop_signal()
                                      .expect("Get StopSignal failed") {
-            elevator.set_motor_dir(MotorDir::Stop)
+            elevator.io.set_motor_dir(MotorDir::Stop)
                    .expect("Set MotorDir failed");
             return;
-        } */
+        }
     }
 }
